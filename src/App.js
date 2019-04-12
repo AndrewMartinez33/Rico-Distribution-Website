53dba195931ce64b64b33b9031f50c49f3444c86
Andrew A. Martinez <andrew.a.martinez33@gmail.com>
2019-04-12 08:33:03 -0700

    Added routing and redirect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Join from "./Components/Join";
+import Products from "./Components/Products";
+import Water from "./Components/Water";
+import NotFound from "./Components/NotFound";
+import Home from "./Components/Home";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Site">
+        <Navbar />
+        <main className="Site-content">
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/join" component={Join} />
+            <Route path="/products" component={Products} />
+            <Route path="/water" component={Water} />
+            <Route path="/not-found" exact component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+        <Footer />
       </div>
     );
   }
