ddf25c9297cf3c390393f9b446b18b93389eaa71
Andrew A. Martinez <andrew.a.martinez33@gmail.com>
2019-04-13 00:12:13 -0700

    added Alkaline, Kangen, and Testimonials components

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Hero from "./Homepage/Hero";
+import Alkaline from "./Homepage/AlkalineWater";
+import Kangen from "./Homepage/KangenWater";
+import Testimonials from "./Homepage/Testimonials";
 
 const Home = () => {
-  return <h1>HomeContent</h1>;
+  return (
+    <React.Fragment>
+      <Hero />
+      <Alkaline />
+      <Kangen />
+      <Testimonials />
+    </React.Fragment>
+  );
 };
 
 export default Home;
