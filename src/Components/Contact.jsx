167183b14b4bd5f6ba0604ddbed7150bff0de2e9
Andrew A. Martinez <andrew.a.martinez33@gmail.com>
2019-04-23 13:27:59 -0700

    added Contact section component

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ContactSection from "./Contactpage/ContactSection";
 
 const Contact = () => {
-  return <h1>Contact</h1>;
+  return <ContactSection />;
 };
 
 export default Contact;
