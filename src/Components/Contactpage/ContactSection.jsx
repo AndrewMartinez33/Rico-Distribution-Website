219b44e06d34836457427f461a7a331721150ee1
Andrew A. Martinez <andrew.a.martinez33@gmail.com>
2019-04-24 15:41:44 -0700

    added contact form

--- src/Components/Contactpage/ContactSection.jsx
+++ src/Components/Contactpage/ContactSection.jsx
@@ -5,35 +5,61 @@ import "./ContactSection.css";
 const ContactSection = () => {
   return (
     <div>
+      <div className="centered-column contact-form-container">
+        <div className="contact-form-header">
+          <h1 className="contact-title">Contact Us</h1>
+          <p className="contact-text">
+            If you have any question or you are ready to start a free trial,
+            book an in-home demo, or order your Kangen Water ® machine, I am
+            here to help guide your through the process.
+          </p>
+          <p className="contact-text">Or give us a call: (555) 555-0000</p>
+        </div>
+      </div>
       <form
-        className="footer-inputs-container"
+        className="centered-column contact-form-container"
         action="formspree email action"
         method="POST"
       >
-        <div className="centered-row footer-inputs-container">
+        <input
+          className="contact-form-inputs"
+          id="name"
+          type="text"
+          placeholder="Name"
+          name="name"
+          required
+        />
+        <input
+          className="contact-form-inputs"
+          id="email"
+          type="email"
+          placeholder="Email"
+          name="_replyto"
+          required
+        />
+        <input
+          className="contact-form-inputs"
+          id="subject"
+          type="text"
+          placeholder="Subject"
+          name="subject"
+        />
+        <textarea
+          className="contact-form-inputs"
+          id="message"
+          type="text"
+          placeholder="Message"
+          name="message"
+          required
+        />
+        <div className="contact-button-container">
           <input
-            className="footer-form-inputs"
-            id="name"
-            type="text"
-            placeholder="Name"
-            name="name"
-            required
-          />
-          <input
-            className="footer-form-inputs"
-            id="email"
-            type="email"
-            placeholder="Email"
-            name="_replyto"
-            required
+            className="btn btn-danger"
+            id="contact-submit"
+            type="submit"
+            value="Send"
           />
         </div>
-        <input
-          className="btn btn-danger footer-submit"
-          id="contact-submit"
-          type="submit"
-          value="Subscribe"
-        />
       </form>
     </div>
   );
